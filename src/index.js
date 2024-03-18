e1d58b2c59e461da8836e090545b0456ebc2c83d
Arthy <arthy.2022@vitstudent.ac.in>
2024-03-18 15:07:35 +0530

    updated ml and css

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const collection = require("./config");
+const bodyParser = require("body-parser");
+const { spawn } = require("child_process");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -83,6 +87,44 @@ app.post("/login", async (req, res) => {
     console.error("Error during login:", error);
     res.status(500).send("Internal Server Error");
   }
+});
+app.get("/predict", (req, res) => {
+  res.render("predict");
+});
+app.post("/predict", (req, res) => {
+  // Extract data from the form submission
+
+  const data = {
+    day: req.body.day,
+    time: req.body.time,
+    mood: req.body.mood,
+    activity: req.body.activity,
+  };
+  const args = Object.values(data);
+  // Execute a Python script with the user input
+  const pythonProcess = spawn("python", ["daily_tracker.py", ...args]);
+
+  // Collect data from the Python script
+  let dataFromPython = "";
+
+  pythonProcess.stdout.on("data", (data) => {
+    dataFromPython += data.toString();
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  pythonProcess.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+    // Send back response to the client
+    res.send(`Result from Python script: ${dataFromPython}`);
+  });
+  pythonProcess.on("error", (err) => {
+    console.error(`Error spawning Python process: ${err}`);
+    // You may want to send an error response to the client if an error occurs
+    // res.status(500).send("An error occurred while processing your request.");
+  });
 });
 
 const port = 3000;
